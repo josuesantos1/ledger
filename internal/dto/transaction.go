3f8a09482e67fb95bedd4d8b2dba0a29fa9289b9
josue santos <josue.santos.dev@gmail.com>
2025-07-19 15:44:42 -0300

    added dto transaction

---
+++ internal/dto/transaction.go
@@ -0,0 +1,18 @@
+package dto
+
+type Amount struct {
+	Value          float64 `json:"value"`
+	Currency       string  `json:"currency"`
+	Fee            float64 `json:"fee"`
+	ConversionRate float64 `json:"conversion_rate,omitempty"`
+}
+
+type Transaction struct {
+	ID              string `json:"id"`
+	TransactionType string `json:"transaction_type"`
+	TransactionId   string `json:"transaction_id"`
+	DebitAmount     Amount `json:"debit_amount"`
+	CreditAmount    Amount `json:"credit_amount"`
+	TransactionDate string `json:"transaction_date"`
+	AccountID       string `json:"account_id"`
+}
